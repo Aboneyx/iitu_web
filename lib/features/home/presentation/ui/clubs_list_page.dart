77f64d5aa0e5614e087e116d338ef232d47a03de
Aboneyx <abylay.asylbek02@gmail.com>
2022-11-18 16:11:29 +0600

    govno

--- lib/features/home/presentation/ui/clubs_list_page.dart
+++ lib/features/home/presentation/ui/clubs_list_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:iitu_web/core/extension/extensions.dart';
+import 'package:iitu_web/core/resources/constants.dart';
 import 'package:iitu_web/features/app/widgets/custom/custom_snackbars.dart';
 import 'package:iitu_web/features/home/data/model/club_dto.dart';
 import 'package:iitu_web/features/home/presentation/bloc/clubs_cubit.dart';
@@ -52,61 +54,96 @@ class _ClubsListPageState extends State<ClubsListPage> {
 
   @override
   Widget build(BuildContext context) {
+    final carouselWidth = context.mediaQuery.size.width * .8;
+
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          children: [
-            // FutureBuilder(
-            //   future: init(),
-            //   builder: (BuildContext context, snapshot) {
-            //     if (snapshot.hasData) {
-            //       if (snapshot.data == null) return const SizedBox();
-            //       return ListView.separated(
-            //         shrinkWrap: true,
-            //         itemCount: snapshot.data!.length,
-            //         itemBuilder: (context, index) {
-            //           return Text(
-            //             snapshot.data![index].data().toString(),
-            //           );
-            //         },
-            //         separatorBuilder: (context, index) => const SizedBox(height: 12),
-            //       );
-            //     }
-
-            //     return const Center(
-            //       child: CircularProgressIndicator(color: Colors.red),
-            //     );
-            //   },
-            // ),
-            BlocConsumer<ClubsCubit, ClubsState>(
-              listener: (context, state) {
-                state.whenOrNull(
-                  errorState: (String message) {
-                    buildErrorCustomSnackBar(context, message);
-                  },
-                );
+        child: BlocConsumer<ClubsCubit, ClubsState>(
+          listener: (context, state) {
+            state.whenOrNull(
+              errorState: (String message) {
+                buildErrorCustomSnackBar(context, message);
               },
-              builder: (context, state) {
-                return state.maybeWhen(
-                  loadedState: (List<ClubDTO> clubs) {
-                    return ListView.separated(
-                      shrinkWrap: true,
-                      itemCount: clubs.length,
-                      itemBuilder: (context, index) {
-                        return Text(
-                          clubs[index].toString(),
-                        );
-                      },
-                      separatorBuilder: (context, index) => const SizedBox(height: 12),
+            );
+          },
+          builder: (context, state) {
+            return state.maybeWhen(
+              loadedState: (List<ClubDTO> clubs) {
+                return ListView.separated(
+                  padding: const EdgeInsets.all(20),
+                  itemCount: clubs.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Text(
+                          clubs[index].name ?? '',
+                          style: const TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        Text(
+                          clubs[index].description ?? '',
+                          style: const TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Container(
+                          width: carouselWidth,
+                          height: carouselWidth * 9 / 16,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            boxShadow: const [
+                              BoxShadow(
+                                offset: Offset(0, 2),
+                                blurRadius: 24,
+                                color: Color.fromRGBO(0, 0, 0, 0.08),
+                              ),
+                              BoxShadow(
+                                blurRadius: 2,
+                                color: Color.fromRGBO(0, 0, 0, 0.08),
+                              ),
+                            ],
+                          ),
+                          child: Image.network(
+                            clubs[index].avatar ?? '',
+                            fit: BoxFit.cover,
+                            loadingBuilder: (context, child, loadingProgress) {
+                              if (loadingProgress == null) {
+                                return child;
+                              }
+                              return Center(
+                                child: CircularProgressIndicator(
+                                  color: AppColors.kPrimaryColor,
+                                  value: loadingProgress.expectedTotalBytes != null
+                                      ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                                      : null,
+                                ),
+                              );
+                            },
+                            errorBuilder: (
+                              BuildContext context,
+                              Object exception,
+                              StackTrace? stackTrace,
+                            ) {
+                              return Center(child: Text('Image Error: $exception'));
+                            },
+                          ),
+                        ),
+                      ],
                     );
                   },
-                  orElse: () => const Center(
-                    child: CircularProgressIndicator(color: Colors.red),
-                  ),
+                  separatorBuilder: (context, index) => const SizedBox(height: 100),
                 );
               },
-            ),
-          ],
+              orElse: () => const Center(
+                child: CircularProgressIndicator(color: Colors.red),
+              ),
+            );
+          },
         ),
       ),
     );
